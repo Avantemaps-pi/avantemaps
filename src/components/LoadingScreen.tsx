08e2fd0eab155b6285d748d9e4c68150fdbd40aa
Avantemaps-pi <avantemaps.pi@gmail.com>
2025-05-12 00:17:38 +0200

    Update and rename LoadingScreen.jsx to LoadingScreen.tsx

---
+++ src/components/LoadingScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './LoadingScreen.css';
+
+const LoadingScreen: React.FC = () => (
+  <div className="loading-screen">
+    <img src="/AvanteMapsIcon.png" alt="Loading..." className="loading-icon" />
+  </div>
+);
+
+export default LoadingScreen;
