822fd66d6384543caedfa932b8b56c68d9efe4b7
Avantemaps-pi <avantemaps.pi@gmail.com>
2025-06-17 22:54:05 +0200

    Update ContactTab.tsx

--- src/components/business/registration/ContactTab.tsx
+++ src/components/business/registration/ContactTab.tsx
@@ -142,7 +142,7 @@ const ContactTab: React.FC<ContactTabProps> = ({ onNext, onPrevious, disabled })
           name="website"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Pi Website URL (Optional)</FormLabel>
+              <FormLabel>Pi Network Website URL (Optional)</FormLabel>
               <FormControl>
                 <Input 
                   placeholder="https://pi-network.example.com" 
